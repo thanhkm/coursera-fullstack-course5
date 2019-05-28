b0e8c525a93e6cd312f13f3af0a2ab9a5aa6a630
KHONG Minh Thanh <thanhkhongminh@gmail.com>
2019-05-28 18:37:12 +0200

    Assignment 1 solution final

--- assignment1/solution/js/app.js
+++ assignment1/solution/js/app.js
@@ -12,8 +12,8 @@ function LunchCheckController ($scope) {
   $scope.DishesCheck = function () {
     var meals = $scope.dishes.split(',');
     var nbMeals = meals.length;
-    if (nbMeals === 0) {
-      $scope.message = "Nothing!";
+    if ($scope.dishes === "") {
+      $scope.message = "Please enter data first";
     } else if (nbMeals <= 3) {
       $scope.message = "Enjoy!";
     } else {
